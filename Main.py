2cb91935292be3ca8aa7dba44b87a33da7b49eac
MarkWinter1 <marknwinter01@gmail.com>
2023-02-19 23:25:12 +0000

    Starter work
    Added in the function in question, graphs it.

--- Main.py
+++ Main.py
@@ -1,6 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy
 
-plt.plot([1,2],[3,4])
-
 print("hello")
+
+f = lambda x: (numpy.e)**x * numpy.cos(10*x)
+
+xVals = numpy.linspace(-(numpy.pi), (numpy.pi), 500)
+yVals = f(xVals)
+
+plot = plt.plot(xVals,yVals)
+#plt.savefig("testoutputplot.png")  ##testplot, just graphing the given function 
+plt.show()
+
+
