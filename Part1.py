65a04c5dc48273f2492b4ffa4bb0e354d19bfae7
Greg523 <63348559+Greg523@users.noreply.github.com>
2023-02-22 17:48:23 +0000

    Update Part1.py

--- Part1.py
+++ Part1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np, numpy
 
-
+# Testing
 ###############  PART ONE  ################
 
 # Create a function which performs a piecewise Lagrange polynomial interpolation. It
